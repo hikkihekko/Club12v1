ed5df765f686d4f81fcbfc6c5d30ad950b08a2f9
hikkihekko <114881550+hikkihekko@users.noreply.github.com>
2023-04-14 00:36:42 +0600

    about block

--- src/components/buy-invest/buy-invest.jsx
+++ src/components/buy-invest/buy-invest.jsx
@@ -10,17 +10,21 @@ function BuyInvest() {
             <div className="buy-invest-container">
                 <div className="buy-block">
                     <h3>Купить кваритру</h3>
-                    <p>При покупке 100% - </p> <p>1 000 000 тг квм </p>
-                    <p>При покупке 50% - </p> <p> 1 200 000 тг квм</p>
+                <div className="text-container">
+                    <p>При покупке 100% - 1 000 000 тг квм </p>
+                    <p>При покупке 50% -  1 200 000 тг квм</p>
+                </div>
                 </div>
             </div>
             <div className="buy-invest-container ">
                 <div className="invest-block">
                     <h3>Цена инвестиции:</h3>
-                    <p>Инвестиционное предложение <br/> для ранних инвесторов </p> <p> 150 000 000 тенге </p>
-                    <p>(минимальная сумма от 5 000 000)</p>
-                    <p>Доходность 30% </p>
-                    <button onClick={()=> setModalActive(true)}>Связаться с нами</button>
+                    <div className="text-container">
+                        <p>Инвестиционное предложение <br/> для ранних инвесторов </p> <p> 150 000 000 тенге </p>
+                        <p>(минимальная сумма от 5 000 000)</p>
+                        <p>Доходность 30% </p>
+                        <button onClick={()=> setModalActive(true)}>Связаться с нами</button>
+                    </div>
                     <Modal active={ModalActive} setActive={setModalActive}/>
                 </div>
             </div>
